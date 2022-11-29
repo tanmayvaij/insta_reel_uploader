5a830e796ea8e7861b351753189f9ba790cad353
tanmayvaij <tanmayvaij22@gmail.com>
2022-11-29 21:12:29 +0530

    added quasar project

---
+++ client/src/layouts/MainLayout.vue
@@ -0,0 +1,19 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+
+    <q-header>
+      <q-toolbar>
+
+        <q-toolbar-title>
+          Instagram Reel Uploader
+        </q-toolbar-title>
+
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+  </q-layout>
+</template>
