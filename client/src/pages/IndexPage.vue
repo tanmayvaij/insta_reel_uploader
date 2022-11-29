08f87c139efeb87447481bc58ba391419025325a
tanmayvaij <tanmayvaij22@gmail.com>
2022-11-29 22:24:55 +0530

    added username password fields

--- client/src/pages/IndexPage.vue
+++ client/src/pages/IndexPage.vue
@@ -1,9 +1,33 @@
 <template>
   <q-page class="flex flex-center">
-    
+    <div>
+
+      <div>
+        <q-input style="margin-bottom:15px;" v-model="username" placeholder="Enter username" filled type="text" />
+      </div>
+
+      <div>
+        <q-input v-model="password" placeholder="Enter password" filled :type="isPwd ? 'password' : 'text'">
+          <template v-slot:append>
+            <q-icon :name="isPwd ? 'visibility_off' : 'visibility'" class="cursor-pointer" @click="isPwd = !isPwd" />
+          </template>
+        </q-input>
+      </div>
+
+    </div>
   </q-page>
 </template>
 
-<script>
 
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup () {
+    return {
+      username: ref(''),
+      isPwd: ref(true)
+    }
+  }
+}
 </script>
---
+++ client/src/pages/ReelUpload.vue
@@ -0,0 +1,10 @@
+<template>
+    <q-page class="flex flex-center">
+      Reel page
+    </q-page>
+  </template>
+  
+  <script>
+  
+  </script>
+  
